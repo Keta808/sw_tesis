da5d0e5086bbe476d1bd5d8169e6ce9e29fd8475
dylan <dylan.suazo2101@alumnos.ubiobio.cl>
2024-07-25 21:49:01 -0400

    Delete comments on pyme.routes

--- backend/src/routes/pyme.routes.js
+++ backend/src/routes/pyme.routes.js
@@ -12,13 +12,13 @@ router.use(authenticationMiddleware);
 
 router.get("/", pymeController.getPymes);
 router.post("/", isAdmin, pymeController.createPyme); // agregar mas roles 
-router.get("/categoria/:categoria", pymeController.getPymesByCategory); // probar 
-router.get("/categoria/:categoria/comuna/:comuna", pymeController.getPymesByCategoryAndComuna); // probar   
-router.get("/nombre/:nombre", pymeController.getPymesByName); // modificar
-router.get("/comuna/:comuna", pymeController.getPymesByComuna); // revisar o eliminar
+router.get("/categoria/:categoria", pymeController.getPymesByCategory);
+router.get("/categoria/:categoria/comuna/:comuna", pymeController.getPymesByCategoryAndComuna); 
+router.get("/nombre/:nombre", pymeController.getPymesByName);
+router.get("/comuna/:comuna", pymeController.getPymesByComuna);
 router.get("/:id", pymeController.getPymeById);
 router.put("/:id", isAdmin, pymeController.updatePyme); // agregar mas roles
-router.delete("/:id", isAdmin, pymeController.deletePyme); // agregar mas roles 
+router.delete("/:id", isAdmin, pymeController.deletePyme); // agregar mas roles
 
 
 export default router;
